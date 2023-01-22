db4afbb892093e6d77b72f238b24631f7e6e3bcf
Rob Ranf <rob@emiyaconsulting.com>
2023-01-22 10:55:26 -0800

    Adding serialization for categoruy id

--- MyAnimangaHeadCanon/MyAnimangaAppLibrary/Models/CategoryModel.cs
+++ MyAnimangaHeadCanon/MyAnimangaAppLibrary/Models/CategoryModel.cs
@@ -1,6 +1,10 @@
-﻿namespace MyAnimangaAppLibrary.Models;
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace MyAnimangaAppLibrary.Models;
 internal class CategoryModel
 {
+    [BsonId]
+    [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
     public string CategoryId { get; set; }
     public string CategoryName { get; set; }
     public string  CategoryDescription{ get; set; }
