86726e0986011a1cdd8d8509764000b206010a49
Rob Ranf <rob@emiyaconsulting.com>
2023-02-07 11:35:30 -0800

    Mongo

--- MyAnimangaHeadCanon/MyAnimangaAppLibrary/DataAccess/MongoUserData.cs
+++ MyAnimangaHeadCanon/MyAnimangaAppLibrary/DataAccess/MongoUserData.cs
@@ -7,7 +7,7 @@ namespace MyAnimangaAppLibrary.DataAccess
     public class MongoUserData
     {
         private readonly IMongoCollection<UserModel> _users;
-
+        
         public MongoUserData(IDbConnection db)
         {
             _users = db.UserCollection;
