3c94891219cc277ff38e2ab9782abff3b1efc6a2
Rob Ranf <rob@emiyaconsulting.com>
2023-02-12 20:49:01 -0800

    Created statuses controllers

---
+++ MyAnimangaHeadCanon/MyAnimangaAppLibrary/DataAccess/IStatusData.cs
@@ -0,0 +1,15 @@
+﻿/** 
+* MongoStatusData.cs
+* Controllers library for Status collection
+* rlr52 | rob@emiyaconsulting.com
+* net6.0
+* Created 2/12/2023 8:25:00 PM
+*/
+
+namespace MyAnimangaAppLibrary.DataAccess;
+
+public interface IStatusData
+{
+    Task CreateStatus(StatusModel status);
+    Task<List<StatusModel>> GetAllStatuses();
+}
---
+++ MyAnimangaHeadCanon/MyAnimangaAppLibrary/DataAccess/MongoStatusData.cs
@@ -0,0 +1,41 @@
+﻿/** 
+* MongoStatusData.cs
+* Controllers library for Status collection
+* rlr52 | rob@emiyaconsulting.com
+* net6.0
+* Created 2/12/2023 8:25:00 PM
+*/
+
+using Microsoft.Extensions.Caching.Memory;
+
+namespace MyAnimangaAppLibrary.DataAccess;
+public class MongoStatusData : IStatusData
+{
+    private readonly IMongoCollection<StatusModel> _statuses;
+    private readonly IMemoryCache _cache;
+    private const string cacheName = "StatusData";
+
+    public MongoStatusData(IDbConnection db, IMemoryCache cache)
+    {
+        _cache = cache;
+        _statuses = db.StatusCollection;
+    }
+
+    public async Task<List<StatusModel>> GetAllStatuses()
+    {
+        var output = _cache.Get<List<StatusModel>>(cacheName);
+        if (output is null)
+        {
+            var results = await _statuses.FindAsync(_ => true);
+            output = results.ToList();
+
+            _cache.Set(cacheName, output, TimeSpan.FromDays(1));
+        }
+        return output;
+    }
+
+    public Task CreateStatus(StatusModel status)
+    {
+        return _statuses.InsertOneAsync(status);
+    }
+}
